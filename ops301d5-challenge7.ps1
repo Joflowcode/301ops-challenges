b8e6f0a2ffb073d2b8797e0d74ccba1979912de5
Jo Flow <joflowcode@gmail.com>
2022-09-08 23:52:53 -0700

    challenge setup, pending AD setup

---
+++ ops301d5-challenge7.ps1
@@ -0,0 +1,6 @@
+:: Script: Ops 301 Class 08 Powershell AD Automation
+:: Author: Jo Flow
+:: Date of latest revision: 9/7/2022 
+:: Purpose: Powershell script to add a new user to AD.
+
+:: VVMs down since 9/6. Setting up new DC now.
