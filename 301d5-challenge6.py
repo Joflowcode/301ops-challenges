6cdcfe1e91f49adb40431b6258e8f1a2a944f47c
Jo Flow <joflowcode@gmail.com>
2022-09-12 14:53:07 +0000

    challenge 6 init

---
+++ 301d5-challenge6.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+# Script: Ops 301 Class 06 Ops Challenge Solution
+# Author: Jo Flow
+# Date of latest revision: 9/11/2022
+# Purpose: Bash in Python
+
+
+# import the operating system library
+# import ipaddress
+import os 
+# import ipaddress
+
+# use the os.system to call a bash command (source lecture)
+os.system("ls")
+
+print("This is introduction to using Python in bash")
+
+
+
+# thisComputerIs = ipaddress
+
+
+# print(ipaddress)
+
+
