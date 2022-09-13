6b661097bb241f1360fa5eb6cf20c6fe44f6662e
Jo Flow <joflowcode@gmail.com>
2022-09-13 06:16:20 +0000

    challenge 10 init

--- ops301d5-challenge10.py
+++ ops301d5-challenge10.py
@@ -6,3 +6,45 @@
 # Purpose: Python  Conditionals
 
 # Main
+
+# variables 
+
+kids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+teenagers = [13, 14, 15, 16, 17, 18, 19, 20]
+adults = 21
+
+age = 0
+
+# Equals: a == b
+
+suzieQ = 10
+billyBob = 4
+
+if age < 13:
+    print('This is the age of a kid')
+
+elif age > 12:
+    print('This is a teenager')
+
+elif age > 20:
+    print('This is an adult')
+
+else:
+    print('Please input a valid number.')
+
+
+
+
+
+
+
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions are not met.
+
+# Create an if statement that includes both elif and else to execute when both if and elif are not met.
+
