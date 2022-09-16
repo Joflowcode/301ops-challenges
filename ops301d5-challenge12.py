d2076cd71255956461f4a5d2ce08a01eeaebd481
Jo Flow <joflowcode@gmail.com>
2022-09-16 15:37:15 +0000

    challenge 12 init

---
+++ ops301d5-challenge12.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# Script: Ops 301 Class 12 Ops Challenge Solution
+# Author: Jo Flow
+# Date of latest revision: 9/15/2022
+# Purpose: Psutil
+
+import psutil 
+
+
+# this will list all the cpu times
+print(psutil.cpu_times())
+
+
+# cpuTimesFile = ("cpu", "w")
+
+
+#Time spent by normal processes executing in user mode
+
+#Time spent by processes executing in kernel mode
+
+#Time when system was idle
+
+#Time spent by priority processes executing in user mode
+
+#Time spent waiting for I/O to complete.
+
+#Time spent for servicing hardware interrupts
+
+#Time spent for servicing software interrupts
+
+
+#Time spent by other operating systems running in a virtualized environment
+#Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
