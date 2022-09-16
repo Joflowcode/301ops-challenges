98587beba9d1e994540b24860d22c5c0d2073594
Jo Flow <joflowcode@gmail.com>
2022-09-16 15:49:11 +0000

    cleaned up comments

--- ops301d5-challenge12.py
+++ ops301d5-challenge12.py
@@ -8,27 +8,6 @@
 import psutil 
 
 
-# this will list all the cpu times
+# this method will list all the cpu times
 print(psutil.cpu_times())
 
-
-# cpuTimesFile = ("cpu", "w")
-
-
-#Time spent by normal processes executing in user mode
-
-#Time spent by processes executing in kernel mode
-
-#Time when system was idle
-
-#Time spent by priority processes executing in user mode
-
-#Time spent waiting for I/O to complete.
-
-#Time spent for servicing hardware interrupts
-
-#Time spent for servicing software interrupts
-
-
-#Time spent by other operating systems running in a virtualized environment
-#Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
